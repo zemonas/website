2d700a4237a50a80f6320aa382533a2d1dc20e24
Abdullah Ajaz Siddiqui <abdulajaz20@gmail.com>
2021-03-15 09:41:48 +0530

    BIG update
    changes{
    1. Added section 2 and section 3 in html
    2. Started working on 2nd page
        (thats currently visible)
        2.1)added div
        2.2)added div styles
    3. Almost completed 3rd page
        (need few tweaking on java side )
        3.1) added div
        3.2) added css
        3.4) added slick carousel
    }
    Removed{
    1. Unnecessary files
       1.1)from main folder
       1.2) javascript files
       1.3)extra images
    }
    Added{
    1. Slick carousel
       1.1) added slideshow in section 3 using this
    2. Custom js
    3. extra css file to work on section 2
    }
    to do{
    1. Figure out how to lay txt section under pad-container between photo and nav bar
    2. figure out lines height in section 2
    3. Figure out why slides are over lapping in section 3
    4. Add a website plan in read me file
    5. Add section 2  css file into main and figure out the transition between section
    }

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+$('.section-3').slick({
+  dots: true,
+  speed: 300,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
