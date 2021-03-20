9a331287b58f59eefa92fa2d41f9052cb33c5011
Abdullah Ajaz Siddiqui <abdulajaz20@gmail.com>
2021-03-20 14:26:29 +0530

    completing commitment one by one
    1. FigureD out how to lay txt section under pad-container between photo and nav bar
    2. FigureD out lines height in section 2
    3. FigureD out why slides are over lapping in section 3
    4. didn't merge the sections yet instead separated them as it would be easy to work on that section
    5. merged all pages using js(all link and nav is working)
    6. removed unnecessary files
    7. cleaned code a little bit
    
    to do{
    1.there is a problem in 1st page background
    2.need to make 2nd and 3rd section/page nav bar
    3. need to fix photo container position after  it wraps down due to screen size
    4. need to complete slides view
    }

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,32 @@
 $('.section-3').slick({
   dots: true,
   speed: 300,
-  centerMode: true,
-  centerPadding: '60px',
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1600,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 1295,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1,
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 700,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
         slidesToShow: 1,
         slidesToScroll: 1
       }
@@ -42,3 +36,69 @@ $('.section-3').slick({
     // instead of a settings object
   ]
 });
+//menu/navbar
+const toggleButton = document.getElementsByClassName('menu')[0];
+const navlist = document.getElementsByClassName('list')[0];
+
+toggleButton.addEventListener('click', () => {
+  $(navlist).toggle("closed");
+  $('.content-box').toggleClass("adjust")
+});
+
+$('#info' ).click(function () {
+  $('.section-2').css('display','none');
+  $('.section-3').css('display','none');
+  $('.header').css('display','');
+  $('body').removeClass('sec-2');
+  $('body').removeClass('sec-3');
+  $('body').addClass('sec-1');
+  $('.section-1').css('display','');
+})
+
+$('#about , #another-about' ).click(function () {
+  $('.section-1').css('display','none');
+  $('.section-3').css('display','none');
+  $('.header').css('display','none');
+  $('body').removeClass('sec-1');
+  $('body').removeClass('sec-3');
+  $('body').addClass('sec-2');
+  $('.section-2').css('display','');
+})
+$('#work' ).click(function () {
+  $('.section-1').css('display','none');
+  $('.section-2').css('display','none');
+  $('.header').css('display','none');
+  $('body').removeClass('sec-1');
+  $('body').removeClass('sec-2');
+  $('body').addClass('sec-3');
+  $('.section-3').css('display','');
+})
+
+
+
+const view = document.getElementsByClassName('pre');
+
+// ES6 arrow function expression and also making use of ES6 template strings
+var log = (msg) => view[0].insertAdjacentHTML('beforeend', `${msg}`);
+
+var letterCount = 0;
+var sentenceCount = 0;
+
+var type = function(message) {
+    sentenceCount++;
+    setTimeout(function() {
+        message.forEach(function (character) {
+            letterCount++;
+            setTimeout(function() {
+                log(character);
+            }, 65 * letterCount);
+        });
+    }, 1000 * sentenceCount);
+}
+
+type('This is like notice board\n\n'.split(''));
+type('HELLO\n\n'.split(''));
+type('A STRANGE GAME.\n'.split(''));
+type('THE ONLY WINNING MOVE IS\n'.split(''));
+type('NOT TO PLAY.\n\n\n'.split(''));
+type('HOW ABOUT A NICE GAME OF\nCHESS?'.split(''));
